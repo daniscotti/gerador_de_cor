fd46c7800805a85489d2961d8d9813968e6004b9
Daniel Fernandes da Silva <danfdasilva@gmail.com>
2023-01-17 10:57:49 -0300

    Criação do projeto

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let arrayDeCoresDaFunção = ["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f"]
+
+function trocaCor () {
+    corAleatória = "#";
+    for (let i=0; i < 6; i++) {
+        let algarismo = Math.floor(Math.random() * 16);
+        let valor = arrayDeCoresDaFunção[algarismo];
+        corAleatória += valor;
+    }
+    document.querySelector("body").style.background = corAleatória;
+}
